e3bf7587080ea2f9915e2d7008bcfd2c887f8746
davyleroy <d.nkurunziz@alustudent.com>
2023-07-08 10:29:24 +0000

    PROGRAMING_IS_COOL

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+        print(f"Failed to retrieve TODO list for employee {employee_id}.")
+n script that returns TODO list progress for a given employee ID
+"""
+import requests
+import json
+
+
+if __name__ == "__main__":
+    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+
+    if response.status_code == 200:
+        todos = response.json()
+
+        completed_tasks = [todo for todo in todos if todo['completed']]
+
+        employee_name = todos[0]['username']
+        number_of_done_tasks = len(completed_tasks)
+        total_number_of_tasks = len(todos)
+
+        print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
+
+        for task in completed_tasks:
+            print(f"\t{task['title']}")
+
+    else:
+        print(f"Failed to retrieve TODO list for employee {employee_id}.")
