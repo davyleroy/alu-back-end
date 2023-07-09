7e7d64c2957bcbf0507720db8fd873a3f7ee8fdc
davyleroy <d.nkurunziz@alustudent.com>
2023-07-09 10:38:29 +0000

    PROGRAMING_IS_COOL

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,24 @@
-
 #!/usr/bin/python3
-"""Module"""
+"""get TODO list"""
 
+import json
 import requests
 import sys
-
-"""Module"""
-
-if __name__ == '__main__':
-    """IF SCRIPT IS NOT RUN AS MODULE"""
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
-
-    user_info = requests.get(user_url).json()
-    todos_info = requests.get(todos_url).json()
-
-    employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
-
-    print("Employee {} is done with tasks({}/{}):".
-          format(employee_name, number_of_done_tasks, total_number_of_tasks))
-
-    [print("\t " + task["title"]) for task in task_completed]
+if __name__ == "__main__":
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(link)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(link)
+    todos = json.loads(res.text)
+    done = []
+    for i in todos:
+        if i['completed']:
+            done.append(i)
+    print("Employee {} is done with tasks({}/{}):".format(
+                                                          user['name'],
+                                                          len(done),
+                                                          len(todos)))
+    for i in done:
+        print("\t {}".format(i["title"]))
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
+"""get TODO list"""
+
 import csv
 import json
 import requests
 import sys
-
-
 if __name__ == "__main__":
-    def export_employee_todo_csv(employee_id):
-    """Make a GET request to the API endpoint"""
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
-
-    if response.status_code == 200:
-        todos = response.json()
-
- 
-        task_data = []
-        for task in todos:
-            task_data.append([
-                task['userId'],
-                task['username'],
-                str(task['completed']),
-                task['title']
-            ])
-
-        
-        file_name = f"{employee_id}.csv"
-
-       
-        with open(file_name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            writer.writerows(task_data)
-
-        print(f"TODO list exported to {file_name} successfully.")
-
-    else:
-        print(f"Failed to retrieve TODO list for employee {employee_id}.")
-
-
-employee_id = int(input("Enter the employee ID: "))
-export_employee_todo_csv(employee_id)
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(link)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(link)
+    todos = json.loads(res.text)
+    csv_data = [["{}".format(i["userId"]),
+                user["username"],
+                "{}".format(i["completed"]),
+                 i["title"]] for i in todos]
+    with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(csv_data)
